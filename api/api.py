fcd6a0920367aebf2806c94185b6482aada37804
Yury Zharikov <zharikovyuri@gmail.com>
2023-05-29 07:20:55 +0300

    add messages

--- api/api.py
+++ api/api.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+import messages
+import datetime
 
 app = FastAPI()
 
+date_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World2"}
+    return {'message': f'as of {date_time}, {messages.TABLEAU_TEST}'}
